b6762a7e7b2d6550e3ae13d8718ebc7d63e626fa
Meze <mezequielabogado@gmail.com>
2023-07-01 19:22:04 -0300

    feat: inicio pagina clinica medica. Home, login

---
+++ js/custom.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById("loginForm").addEventListener("submit", function(event) {
+      event.preventDefault();  
+    
+      var username = document.getElementById("username").value;
+      var password = document.getElementById("password").value;
+     
+      if (username === "mirtita" && password === "123") {
+        window.location.href = "paciente.html?user=" + encodeURIComponent(username);
+      } else if (username === "house" && password === "123") {
+        window.location.href = "doctor.html?user=" + encodeURIComponent(username);
+      } else {
+        alert("Usuario incorrecto");
+      }
+    });
+  });
+  
