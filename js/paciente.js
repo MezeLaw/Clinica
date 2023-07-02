768154788e605e5130410d139848253bb057a535
Meze <mezequielabogado@gmail.com>
2023-07-02 11:37:11 -0300

    fix: se fixea ingreso a paciente para redirigir al login en caso de no localStorage o login valido

--- js/paciente.js
+++ js/paciente.js
@@ -2,10 +2,19 @@ window.onload = function() {
     var url = new URL(window.location.href);
     var params = new URLSearchParams(url.search);
     var user = params.get('user');
- 
-    var saludoPaciente = document.getElementById('saludo-paciente')
-    localStorage.setItem('userLogged', user);
-    saludoPaciente.textContent = 'Hola, ' + user + '!';
+    var userLocalStorage = localStorage.getItem('userLogged');
+
+    if (user) {
+      var saludoPaciente = document.getElementById('saludo-paciente')
+      localStorage.setItem('userLogged', user);
+      saludoPaciente.textContent = 'Hola, ' + user + '!';
+    } else if (userLocalStorage) {
+      var saludoPaciente = document.getElementById('saludo-paciente')
+      localStorage.setItem('userLogged', userLocalStorage);
+      saludoPaciente.textContent = 'Hola, ' + userLocalStorage + '!';
+    } else {
+      window.location.href = "./login.html"
+    }
   };
 
 
