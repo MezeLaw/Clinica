b69037f93c987f2d0415899557ba391862385e94
Meze <mezequielabogado@gmail.com>
2023-07-01 22:58:00 -0300

    feat: WIP doctor. Fix de funcion que levanta creds para evitar error

---
+++ js/login.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById("loginForm").addEventListener("submit", function(event) {
+      event.preventDefault();  
+    
+      var username = document.getElementById("username").value;
+      var password = document.getElementById("password").value;
+     
+      if (username === "mirtita" && password === "123") {
+        localStorage.setItem('userLogged', username);
+        window.location.href = "paciente.html?user=" + encodeURIComponent(username);
+      } else if (username === "house" && password === "123") {
+        localStorage.setItem('userLogged', username);
+        window.location.href = "doctor.html?user=" + encodeURIComponent(username);
+      } else {
+        alert("Usuario incorrecto");
+      }
+    });
+  });
